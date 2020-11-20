9c25f9c1d3348a8395cb7d4144d771060878a929
sgr98 <60283755+sgr98@users.noreply.github.com>
2020-11-21 02:29:17 +0530

    Added colour change to other parameters

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import Interface from './Components/Interface';
 import './App.css';
 import data from './data.json';
 import react, { Component } from 'react';
+import axios from 'axios';
 
 console.log(data);
 
 class App extends Component {
+
+  
+  //Get HTTP URL JSON Data
+  // fetchJsonUrl = () => {
+  //   const res = axios.get('');
+  // }
 
   state = {
     infos : data
--- src/Components/Interface.js
+++ src/Components/Interface.js
@@ -3,7 +3,7 @@ import './Interface.css';
 
 class Interface extends Component {
 
-    displayColor = (temp) => {
+    displayTempColor = (temp) => {
         if(temp < 50) {
             return {background : '#b9fffc'}
         }
@@ -12,6 +12,30 @@ class Interface extends Component {
         }
         else {
             return {background : '#fe5f55'}
+        }
+    }
+
+    displayMoistureColor = (moist) => {
+        if(moist < 50) {
+            return {background : '#f2dcbb'}
+        }
+        else if(moist < 70) {
+            return {background : '#e8e8e8'}
+        }
+        else {
+            return {background : '#51adcf'}
+        }
+    }
+
+    displayPHColor = (ph) => {
+        if(ph < 6) {
+            return {background : '#e4e978'}
+        }
+        else if(ph < 8) {
+            return {background : '#e8e8e8'}
+        }
+        else {
+            return {background : '#c3aed6'}
         }
     }
 
@@ -24,15 +48,15 @@ class Interface extends Component {
                         <h4>Date : {info.date} ; Time : {info.time}</h4>
                     </div>
 
-                    <div className="Interface-individual" style={this.displayColor(info.temperature)}>
+                    <div className="Interface-individual" style={this.displayTempColor(info.temperature)}>
                         <h3>Temperature = {info.temperature} &#176;C</h3>
                     </div>
 
-                    <div className="Interface-individual">
+                    <div className="Interface-individual" style={this.displayMoistureColor(info.moisture)}>
                         <h3>Moisture = {info.moisture} %</h3>
                     </div>
 
-                    <div className="Interface-individual" id="ph">
+                    <div className="Interface-individual" style={this.displayPHColor(info.ph)} id="ph">
                         <h3>ph = {info.ph}</h3>
                     </div>
                 </div>
